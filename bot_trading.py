dc5118912a058c992d71e9b4391f069ec0552113
ArKangel15 <ing.jangelha@gmail.com>
2026-01-25 23:43:38 -0700

    Update bot_trading.py

--- bot_trading.py
+++ bot_trading.py
@@ -397,7 +397,7 @@ acciones_mx = [
     "CABLECPO.MX",
     "UBER.MX",
     "GMD.MX"
-    "BWMX.MX"
+    "BWMX"
 ]
 
 # ============================================================
@@ -602,6 +602,7 @@ acciones_usa = [
     "OFLX",
     "SNGX"
 ]
+
 
 
 
